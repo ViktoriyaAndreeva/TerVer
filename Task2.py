d8fbc6b9ecee90bba98272c77d53568a19c7d6e6
Viktoriya <anviktoriya@yandex.ru>
2022-12-18 19:16:05 +0300

    Task2 changed

--- Task2.py
+++ Task2.py
@@ -12,7 +12,7 @@ ver_white_box1 = white_box1 / box1
 ver_not_white_box1 = (box1 - white_box1) / box1
 ver_white_box2 = white_box2 / box2
 ver_not_white_box2 = (box2 - white_box2) / box2
-result_three_white = ver_white_box1*4/7*ver_white_box2*7/11*6/10*5/9 + ver_white_box1 * \
-    3/7*ver_white_box2*4/11*7/10*6/9 + \
-    ver_not_white_box1*2/7*ver_white_box2*4/11*3/10*7/9
+result_three_white = ver_white_box1 * 4/7 * ver_white_box2 * 7/11 * 6/10 * 5/9 + ver_white_box1 * \
+    3/7 * ver_white_box2 * 4/11 * 7/10 * 6/9 + \
+    ver_not_white_box1 * 2/7 * ver_white_box2 * 4/11 * 3/10 * 7/9
 print("Вероятность, что 3 мяча белые:", result_three_white)
